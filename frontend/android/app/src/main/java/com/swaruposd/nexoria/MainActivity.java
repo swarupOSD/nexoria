2e30eb1219b3b0b57bf469fee70f1844e5e43051
Snehashis Roy <snehashisroy106@gmail.com>
2026-07-04 15:25:10 +0530

    feat: Add Android Capacitor Project with Background permissions

---
+++ frontend/android/app/src/main/java/com/swaruposd/nexoria/MainActivity.java
@@ -0,0 +1,20 @@
+package com.swaruposd.nexoria;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Ensure WebView allows background media playback without explicit user gestures
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            settings.setMediaPlaybackRequiresUserGesture(false);
+        }
+    }
+}
