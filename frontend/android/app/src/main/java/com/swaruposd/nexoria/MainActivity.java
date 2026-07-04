24de460b4bd937b8555769116ea703e44831ae98
Snehashis Roy <snehashisroy106@gmail.com>
2026-07-04 19:33:55 +0530

    fix: Replace Native Android Foreground Service with fixed Android 14 compatible cordova-plugin-background-mode fork

--- frontend/android/app/src/main/java/com/swaruposd/nexoria/MainActivity.java
+++ frontend/android/app/src/main/java/com/swaruposd/nexoria/MainActivity.java
@@ -3,8 +3,6 @@ package com.swaruposd.nexoria;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.content.Intent;
-import android.os.Build;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -17,24 +15,6 @@ public class MainActivity extends BridgeActivity {
         if (webView != null) {
             WebSettings settings = webView.getSettings();
             settings.setMediaPlaybackRequiresUserGesture(false);
-        }
-
-        // Start Foreground Service to keep WebView alive in the background
-        Intent serviceIntent = new Intent(this, BackgroundAudioService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(serviceIntent);
-        } else {
-            startService(serviceIntent);
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        // Prevent Capacitor from pausing the WebView when the app goes to the background
-        if (this.bridge != null && this.bridge.getWebView() != null) {
-            this.bridge.getWebView().resumeTimers();
-            this.bridge.getWebView().onResume();
         }
     }
 }
